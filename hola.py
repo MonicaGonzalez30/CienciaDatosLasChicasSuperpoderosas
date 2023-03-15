068e938128f632073fdfac8dad2cdf3d6759f4e2
Ernesto43010 <luisluna2774408@outlook.com>
2023-03-15 07:45:05 -0600

    Agregue el for en hola.py  para practicar

--- hola.py
+++ hola.py
@@ -2,3 +2,6 @@ print("Hola chicas, inserten un hola de prueba")
 #Ejememplo de que alguien conteste
 print("Moni - Hola Ernesto :3")
 print("Esme - Hola chicas")
+
+for i in range(11):
+    print(i)
