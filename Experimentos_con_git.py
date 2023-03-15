295570fa7db97121e6bb9ab06404c31e3107dedf
Ernesto43010 <luisluna2774408@outlook.com>
2023-03-15 12:03:36 -0600

    Cambie el nombre del archivo

---
+++ Experimentos_con_git.py
@@ -0,0 +1,12 @@
+print("Hola chicas, inserten un hola de prueba")
+#Ejememplo de que alguien conteste
+print("Moni - Hola Ernesto :3")
+print("Esme - Hola chicas")
+
+for i in range(11):
+    print(i)
+
+
+def cubo(x): return x**3
+
+equipo = ['Luisito', 'Monica', 'Esme']
